2544542278e9f6d8e2e1c9ec886d303aa8e6aa19
Antroverden <Antroverden@yandex.ru>
2023-03-31 15:25:36 +0300

    refactor: добавить сервисы для обработки бизнес-логики

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
